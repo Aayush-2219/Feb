4630e64e7aebb48b246e618f73525085bf918004
Aayush-2219 <154007060+Aayush-2219@users.noreply.github.com>
2026-02-14 11:58:37 +0530

    Change app.run to use environment PORT and host

--- App.py
+++ App.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def home():
     return render_template("welcome.html")
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
